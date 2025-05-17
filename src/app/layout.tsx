5efa95627acd53bd3c956ed8cc7f0e9fa8aa285b
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2025-05-17 09:18:41 -0400

    feat: integrate Font Awesome and refactor layout for global icon support
    - Installed @fortawesome/react-fontawesome and related icon packages
    - Created centralized Font Awesome config in lib/fontawesome.ts
    - Imported Font Awesome config in RootLayout to enable global icon usage
    - Cleaned up layout file for consistency and future styling improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import "@/app/lib/fontawesome"; // Import Font Awesome config
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
---
+++ src/app/lib/fontawesome.ts
@@ -0,0 +1,6 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTruck, faArrowRotateLeft, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCircleQuestion } from '@fortawesome/free-regular-svg-icons';
+import { faSpotify, faApple, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faTruck, faArrowRotateLeft, faInfoCircle, faCircleQuestion, faSpotify, faApple, faYoutube);
