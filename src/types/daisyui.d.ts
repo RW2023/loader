b2c856a9e764be3e2441e2bcb92bd5341d8629da
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2025-05-17 10:36:48 -0400

    feat: define DaisyUI module with Tailwind CSS plugin type

--- src/types/daisyui.d.ts
+++ src/types/daisyui.d.ts
@@ -1,1 +1,7 @@
-declare module 'daisyui';
+// types/daisyui.d.ts
+declare module 'daisyui' {
+    import { Plugin } from 'tailwindcss';
+    const plugin: Plugin;
+    export default plugin;
+  }
+  
